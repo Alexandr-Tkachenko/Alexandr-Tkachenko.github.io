fe37e5ac6b768b05c342d8d667cbad2faa0e1c50
Alexandr Tkachenko <zloyyenot.200316@gmail.com>
2018-09-01 18:17:15 +0300

    Fixed page in IE11 #2

--- Waxom/js/common.js
+++ Waxom/js/common.js
@@ -1,191 +1,202 @@
 $(document).ready(function(){
 
-// Header height	
-function headerHeight () {
+	// Header height	
+	function headerHeight () {
 
-	const frontPageTop = document.querySelector('.header');
-	const headerCompositionWrapper = document.querySelector('.header-composition-wrapper');
+		const frontPageTop = document.querySelector('.header');
+		const headerCompositionWrapper = document.querySelector('.header-composition-wrapper');
 
-	if (window.innerWidth <= 1024) {
-		if (frontPageTop) {
-			frontPageTop.style.height = window.innerHeight + 'px';
-		 }
-	};
-
-	if ( $(window).height() <= 530) {
-		if (frontPageTop) {
-			frontPageTop.style.height = 530 + 'px';
-		}
-	};
-
-};
-
-headerHeight ();
-
-window.addEventListener( "resize", headerHeight);
-window.addEventListener( "scroll", headerHeight);
-
-alert('тест')
-
-// Open and close search input
-$('.header-search-button').click( function(){
-	$('.header-search-form').addClass('is-active');
-	$('.hamburger').addClass('is-hidden');
-	$('.header-logo').addClass('is-hidden');	
-	$('.header-nav').addClass('is-hidden');
-	$('.header-icons').addClass('is-hidden');
-	$('.header-search').focus();
-});
-
-$('.header-close-form').click( function(){
-	$('.header-search-form').removeClass('is-active');
-	$('.hamburger').removeClass('is-hidden');
-	$('.header-logo').removeClass('is-hidden');	
-	$('.header-nav').removeClass('is-hidden');	
-	$('.header-icons').removeClass('is-hidden');	
-});
-
-
-// Mmenu
-$("#my-menu").mmenu({
-			 navbar: false,
-			"extensions": [
-					"border-full",
-					"fx-panels-none",
-					 "shadow-panels",
-			 ]
-		}, {
-			 offCanvas: {
-					pageNodetype: "section"
+		if (window.innerWidth <= 1024) {
+			if (frontPageTop) {
+				frontPageTop.style.height = window.innerHeight + 'px';
 			 }
-		});
+		};
 
-var api = $("#my-menu").data( "mmenu" );
+		if ( $(window).height() <= 530) {
+			if (frontPageTop) {
+				frontPageTop.style.height = 530 + 'px';
+			}
+		};
 
-api.bind( "open:start", function() {
-	$('.hamburger').addClass('is-active');
-});
-api.bind( "close:after", function() {
-	$('.hamburger').removeClass('is-active');
-});
+	};
+
+	headerHeight ();
+
+	window.addEventListener( "resize", headerHeight);
+	window.addEventListener( "scroll", headerHeight);
+	
+
+	// Open and close search input
+	$('.header-search-button').click( function(){
+		$('.header-search-form').addClass('is-active');
+		$('.hamburger').addClass('is-hidden');
+		$('.header-logo').addClass('is-hidden');	
+		$('.header-nav').addClass('is-hidden');
+		$('.header-icons').addClass('is-hidden');
+		$('.header-search').focus();
+	});
+
+	$('.header-close-form').click( function(){
+		$('.header-search-form').removeClass('is-active');
+		$('.hamburger').removeClass('is-hidden');
+		$('.header-logo').removeClass('is-hidden');	
+		$('.header-nav').removeClass('is-hidden');	
+		$('.header-icons').removeClass('is-hidden');	
+	});
 
 
-// Owl-carousel
-$(".header-carousel").owlCarousel({
-	items: 1,
-	smartSpeed: 1000,
-	nav: true,
-	loop: true,
-	autoplay: true,
-	autoplayTimeOut: 7500,
-	autoplayHoverPause: true,
-});
+	// Mmenu
+	$("#my-menu").mmenu({
+				 navbar: false,
+				"extensions": [
+						"border-full",
+						"fx-panels-none",
+						 "shadow-panels",
+				 ]
+			}, {
+				 offCanvas: {
+						pageNodetype: "section"
+				 }
+			});
 
-$(".posts-carousel").owlCarousel({
-	smartSpeed: 600,
-	nav: true,
-	loop: true,
-	margin: 30,
-	autoplay: true,
-	autoplayTimeOut: 6000,
-	navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
-	dots: false,
-	autoplayHoverPause: true,
-	responsive: {
+	var api = $("#my-menu").data( "mmenu" );
 
-			0: {
-				items: 1,
-			},
+	api.bind( "open:start", function() {
+		$('.hamburger').addClass('is-active');
+	});
+	api.bind( "close:after", function() {
+		$('.hamburger').removeClass('is-active');
+	});
 
-			500: {
-				items: 2,
-			},
 
-			856: {
-				items: 3,
-			},
+	// Owl-carousel
+	$(".header-carousel").owlCarousel({
+		items: 1,
+		smartSpeed: 1000,
+		nav: true,
+		loop: true,
+		autoplay: true,
+		autoplayTimeOut: 7500,
+		autoplayHoverPause: true,
+	});
+
+	$(".posts-carousel").owlCarousel({
+		smartSpeed: 600,
+		nav: true,
+		loop: true,
+		margin: 30,
+		autoplay: true,
+		autoplayTimeOut: 6000,
+		navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+		dots: false,
+		autoplayHoverPause: true,
+		responsive: {
+
+				0: {
+					items: 1,
+				},
+
+				500: {
+					items: 2,
+				},
+
+				856: {
+					items: 3,
+				},
+
+		}
+	});
+
+	$(".partners-carousel").owlCarousel({
+		smartSpeed: 600,
+		loop: true,
+		margin: 30,
+		autoplay: true,
+		autoplayTimeOut: 8000,
+		items: 4,
+		autoplayHoverPause: true,
+		responsive: {
+
+				0: {
+					items: 1,
+				},
+
+				520: {
+					 items: 2,
+				},
+
+				810: {
+					 items: 3,
+				},
+
+				1060: {
+					 items: 4,
+				},
+
+		}
+	});
+
+
+	// MixItUp
+	var mixer = mixitup('.portfolio-projects');
+
+
+	// Maginific Popup
+	$('.video-play-button').magnificPopup({		
+
+		type: 'iframe',
+		iframe: {
+					markup: '<div class="mfp-iframe-scaler animated zoomIn faster">'+
+									'<div class="mfp-close"></div>'+
+									'<iframe src="https://www.youtube.com/embed/LXnTYNkTsYc?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'+
+									'</div>', 
+		},
+		removalDelay: 300,		
+
+	});
+
+
+	// Excellent photo height
+	function photoHeight () {
+
+		var photoWidth = $('.excellent-photo').width();
+
+		if(document.documentElement.clientWidth < 465) {
+			$('.excellent-photo').css(
+				{height: photoWidth/0.68}
+			);
+		} else {
+			$('.excellent-photo').css(
+				{height: '' }
+			);
+		}
 
 	}
-});
 
-$(".partners-carousel").owlCarousel({
-	smartSpeed: 600,
-	loop: true,
-	margin: 30,
-	autoplay: true,
-	autoplayTimeOut: 8000,
-	items: 4,
-	autoplayHoverPause: true,
-	responsive: {
+	photoHeight();
 
-			0: {
-				items: 1,
-			},
+	window.onresize = photoHeight;
 
-			520: {
-				 items: 2,
-			},
 
-			810: {
-				 items: 3,
-			},
+	// CounterUp
+	$('.count-number').counterUp({
+		time: 3000,
+	});
 
-			1060: {
-				 items: 4,
-			},
 
+	// Wow.js
+	new WOW().init();
+
+
+	// Styles for fucking IE
+	var isIE = false || !!document.documentMode;
+
+	if (isIE) {
+	    var body  = document.getElementsByTagName("body")[0];
+	    var link  = document.createElement("link");
+	    link.rel  = "stylesheet";
+	    link.href = "css/ie.css";
+	    body.appendChild(link);
 	}
-});
-
-
-// MixItUp
-var mixer = mixitup('.portfolio-projects');
-
-
-// Maginific Popup
-$('.video-play-button').magnificPopup({		
-
-	type: 'iframe',
-	iframe: {
-				markup: '<div class="mfp-iframe-scaler animated zoomIn faster">'+
-								'<div class="mfp-close"></div>'+
-								'<iframe src="https://www.youtube.com/embed/LXnTYNkTsYc?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'+
-								'</div>', 
-	},
-	removalDelay: 300,		
-
-});
-
-
-// Excellent photo height
-function photoHeight () {
-
-	var photoWidth = $('.excellent-photo').width();
-
-	if(document.documentElement.clientWidth < 465) {
-		$('.excellent-photo').css(
-			{height: photoWidth/0.68}
-		);
-	} else {
-		$('.excellent-photo').css(
-			{height: '' }
-		);
-	}
-
-}
-
-photoHeight();
-
-window.onresize = photoHeight;
-
-
-// CounterUp
-$('.count-number').counterUp({
-	time: 3000,
-});
-
-
-// Wow.js
-new WOW().init();
 
 });
